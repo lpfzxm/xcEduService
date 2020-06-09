6fdf0aa2fa320beb6864f7c0fda68f35e44ff05b
Li Pengfei <184441376@qq.com>
2020-06-09 23:34:34 +0800

    初次提交学成在线项目代码

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/dao/CmsPageRepository.java
@@ -0,0 +1,13 @@
+package com.xuecheng.manage_cms.dao;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * @author lipengfei <184441376@qq.com><br/>
+ * @date 2020/6/8
+ */
+public interface CmsPageRepository extends MongoRepository<CmsPage,String> {
+}
+
